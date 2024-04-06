e962fcf0983f1bd668c6f9a6fdc7bef6289c8341
dc-21 <cholahkuboko@gmail..com>
2024-04-06 22:17:57 +0200

    server stuff

---
+++ server/src/user/dto.ts
@@ -0,0 +1,54 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastname: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly address: string;
+
+  constructor(d: RegisterDto) {
+    this.username = d.username;
+    this.firstname = d.firstname;
+    this.lastname = d.lastname;
+    this.email = d.email;
+    this.password = d.password;
+    this.phone = d.phone;
+    this.address = d.address;
+  }
+}
+
+export class LoginDto {
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+
+  constructor(d: LoginDto) {
+    this.email = d.email;
+    this.password = d.password;
+  }
+}
