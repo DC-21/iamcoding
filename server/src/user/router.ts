8c1f10841d4f935aa13c5f295bf0969b6fd7f899
dc-21 <cholahkuboko@gmail..com>
2024-04-06 22:47:54 +0200

    auth

---
+++ server/src/user/router.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { UserCollection } from "./collection";
+
+const userCollection = new UserCollection();
+const userRouter = Router();
+
+userRouter.post("/signup", userCollection.Register);
+userRouter.post("/activate", userCollection.activateUser);
+userRouter.put("/avatar/:id", userCollection.Update);
+userRouter.post("/signin", userCollection.Login);
+
+export default userRouter;
