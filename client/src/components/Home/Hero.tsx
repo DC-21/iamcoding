f181a805e501625e805d9852b47b29f55feb562f
dc-21 <cholahkuboko@gmail..com>
2024-04-07 01:19:11 +0200

    navbar

--- client/src/components/Home/Hero.tsx
+++ client/src/components/Home/Hero.tsx
@@ -2,11 +2,34 @@ import bg from "../../assets/software.jpg";
 
 const Hero = () => {
   return (
-    <section
-      style={{ backgroundImage: `url(${bg})` }}
-      className="bg-center bg-cover object-cover bg-no-repeat"
-    >
-      <div className="min-h-screen flex w-full bg-slate-950 bg-opacity-60 backdrop:blur-2x"></div>
+    <section className="">
+      <div className="min-h-screen flex w-full p-10 bg-slate-950 justify-center items-center">
+        <div className="w-full flex justify-center px-24">
+          <div className="w-full flex justify-center items-center p-4 gap-4 bg-white rounded shadow-sm shadow-gray-600">
+            <a href="">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+                height="3em"
+                width="3em"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <path d="M17 10 A7 7 0 0 1 10 17 A7 7 0 0 1 3 10 A7 7 0 0 1 17 10 z" />
+                <path d="M21 21l-6-6" />
+              </svg>
+            </a>
+            <input
+              placeholder="search"
+              className="p-3 outline-none rounded w-full text-lg text-slate-950"
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- client/src/components/global/Navbar.tsx
+++ client/src/components/global/Navbar.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <nav className="p-6 flex w-full justify-between text-white bg-[#06031d] sticky top-0 bg-opacity-100 z-50 backdrop-blur-md">
+    <nav className="px-10 py-4 flex w-full justify-between text-white bg-[#06031d] sticky top-0 bg-opacity-100 z-50 backdrop-blur-md">
       <a className="text-3xl font-bold text-green-500" href="">
         i<span className="text-white">amcoding</span>
       </a>
       <div className="flex gap-4 items-center">
-        <a className=" font-normal" href="">
+        <a className=" font-normal hover:underline" href="">
           learn
         </a>
-        <a className=" font-normal" href="">
+        <a className=" font-normal hover:underline" href="">
           bootcamps
         </a>
-        <a className=" font-normal" href="">
+        <a className=" font-normal hover:underline" href="">
           teach
         </a>
       </div>
