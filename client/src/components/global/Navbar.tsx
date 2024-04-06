072023dea6fde6c8bc634c0e0b3adf239995dc10
dc-21 <cholahkuboko@gmail..com>
2024-04-07 00:26:25 +0200

    navbar

--- client/src/components/global/Navbar.tsx
+++ client/src/components/global/Navbar.tsx
@@ -1,9 +1,20 @@
 const Navbar = () => {
   return (
-    <nav className="p-6 flex w-full justify-between bg-[#06031d]">
-      <a className="text-2xl font-bold text-green-500" href="">
+    <nav className="p-6 flex w-full justify-between text-white bg-[#06031d] sticky top-0 bg-opacity-100 z-50 backdrop-blur-md">
+      <a className="text-3xl font-bold text-green-500" href="">
         i<span className="text-white">amcoding</span>
       </a>
+      <div className="flex gap-4 items-center">
+        <a className=" font-normal" href="">
+          learn
+        </a>
+        <a className=" font-normal" href="">
+          bootcamps
+        </a>
+        <a className=" font-normal" href="">
+          teach
+        </a>
+      </div>
     </nav>
   );
 };
