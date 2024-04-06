3b9a5d9d9924839632c77507e3bd279b9b9fd3ca
dc-21 <cholahkuboko@gmail..com>
2024-04-07 00:18:40 +0200

    navbar

--- client/src/components/global/Navbar.tsx
+++ client/src/components/global/Navbar.tsx
@@ -1,5 +1,11 @@
 const Navbar = () => {
-  return <nav className="p-6 flex w-full justify-between"></nav>;
+  return (
+    <nav className="p-6 flex w-full justify-between bg-[#06031d]">
+      <a className="text-2xl font-bold text-green-500" href="">
+        i<span className="text-white">amcoding</span>
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
