d299162e718460639195dde73ca8f85f2f0e6916
dc-21 <cholahkuboko@gmail..com>
2024-04-06 22:48:23 +0200

    auth

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import compression from "compression";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
+import userRouter from "./user/router";
 
 const app: Application = express();
 const PORT = process.env.PORT || 1911;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 app.use(morgan("dev"));
+
+app.use("/auth", userRouter);
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("IAMCODING is Live");
