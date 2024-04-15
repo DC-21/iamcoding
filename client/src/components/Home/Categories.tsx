42533b32c13b663a5630cee2b5b790f0533f706f
dc-21 <cholahkuboko@gmail..com>
2024-04-15 19:10:05 +0200

    client

--- client/src/components/Home/Categories.tsx
+++ client/src/components/Home/Categories.tsx
@@ -3,6 +3,16 @@ import iot from "../../assets/iot(1).png";
 import cloud_computing from "../../assets/cloud-computing.png";
 import ai from "../../assets/ai.png";
 
+import ml from "../../assets/coding.png";
+import dt from "../../assets/data-science.png";
+import networking from "../../assets/circle.png";
+import graphics from "../../assets/graphic-design.png";
+
+import security from "../../assets/security.png";
+import blockchain from "../../assets/blockchain.png";
+import payment from "../../assets/payment.png";
+import cryptography from "../../assets/private-key.png";
+
 const Categories = () => {
   return (
     <section className="w-full min-h-screen flex flex-col">
@@ -29,32 +39,40 @@ const Categories = () => {
           </div>
         </div>
         <div className="w-full grid md:grid-cols-4 grid-cols-1 gap-6 pt-4">
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Machine Learning</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={ml} className="w-40 h-36" alt="ml" />
+            <p className="pt-2">Machine Learning</p>
           </div>
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Data Science</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={dt} className="w-40 h-36" alt="dt" />
+            <p className="pt-2">Data Science</p>
           </div>
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Networking</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={networking} className="w-40 h-36" alt="networking" />
+            <p className="pt-2">Networking</p>
           </div>
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Graphics and Design</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={graphics} className="w-40 h-36" alt="graphics" />
+            <p className="pt-2">Graphics and Design</p>
           </div>
         </div>
 
         <div className="w-full grid md:grid-cols-4 grid-cols-1 gap-6 pt-4">
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Cyber Security</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={security} className="w-40 h-36" alt="security" />
+            <p className="pt-2">Cyber Security</p>
           </div>
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Blockchain</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={blockchain} className="w-40 h-36" alt="blockchain" />
+            <p className="pt-2">Blockchain</p>
           </div>
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Digital Banking</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={payment} className="w-40 h-36" alt="payment" />
+            <p className="pt-2">Digital Banking</p>
           </div>
-          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
-            <p>Cryptography</p>
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={cryptography} className="w-40 h-36" alt="cryptography" />
+            <p className="pt-2">Cryptography</p>
           </div>
         </div>
       </div>
