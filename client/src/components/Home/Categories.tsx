10075b7a1aa1d0210c90afa7a01ccb863ee598e6
dc-21 <cholahkuboko@gmail..com>
2024-04-15 18:34:04 +0200

    client

--- client/src/components/Home/Categories.tsx
+++ client/src/components/Home/Categories.tsx
@@ -1,5 +1,61 @@
+import programming from "../../assets/programming(1).png";
+import iot from "../../assets/iot(1).png";
+
 const Categories = () => {
-  return <section className="w-full min-h-screen">Categories</section>;
+  return (
+    <section className="w-full min-h-screen flex flex-col">
+      <div className="flex w-full flex-col justify-center items-center md:p-10 p-4">
+        <p className="md:text-[55px] font-bold text-[#06031d]">
+          Our Categories
+        </p>
+        <div className="w-full grid md:grid-cols-4 grid-cols-1 gap-6 pt-4">
+          <div className="w-full cursor-pointer hover:shadow-xl hover:shadow-slate-60 rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-6">
+            <img src={programming} className=" w-40 h-36" alt="programming" />
+            <p className="text-bold pt-2">Programming</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <img src={iot} className=" w-40 h-36" alt="iot" />
+            <p>IoT</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Cloud Computing</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>AI</p>
+          </div>
+        </div>
+        <div className="w-full grid md:grid-cols-4 grid-cols-1 gap-6 pt-4">
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Machine Learning</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Data Science</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Networking</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Graphics and Design</p>
+          </div>
+        </div>
+
+        <div className="w-full grid md:grid-cols-4 grid-cols-1 gap-6 pt-4">
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Cyber Security</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Blockchain</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Digital Banking</p>
+          </div>
+          <div className="w-full rounded-2xl border border-slate-950 shadow-sm shadow-slate-950 justify-center items-center flex flex-col p-3">
+            <p>Cryptography</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
