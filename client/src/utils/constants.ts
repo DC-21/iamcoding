c48d44d0c91220c34ab2146db0e09e1dd1c3fa81
dc-21 <cholahkuboko@gmail..com>
2024-04-20 13:50:43 +0200

    login and sign up stuff

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,3 @@
-export const ENDPOINT: string = "https://iamcoding-server.vercel.app";
+// export const ENDPOINT: string = "https://iamcoding-server.vercel.app";
+
+export const ENDPOINT: string = "http://localhost:1911";
