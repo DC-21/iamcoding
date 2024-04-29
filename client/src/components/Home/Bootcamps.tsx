90b2803db43eef905ef9a76fe1abe83608bb5879
dc-21 <cholahkuboko@gmail..com>
2024-04-29 09:48:52 +0200

    client stuff

--- client/src/components/Home/Bootcamps.tsx
+++ client/src/components/Home/Bootcamps.tsx
@@ -1,9 +1,14 @@
+import bootcamp from "../../assets/bootcamp.jpg";
+
 const Bootcamps = () => {
   return (
-    <div className="md:px-10 px-6 py-4 w-full min-h-screen flex-col justify-center items-center flex">
+    <div className="md:px-10 px-6 py-10 w-full min-h-screen flex-col justify-center items-center flex">
       <p className="md:text-[55px] text-[35px] font-bold text-[#06031d]">
         BootCamps For You
       </p>
+      <div>
+        <img src={bootcamp} alt="bootcamp" />
+      </div>
     </div>
   );
 };
