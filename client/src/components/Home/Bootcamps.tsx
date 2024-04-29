7f665351db3f7cac405a57b811fba646464cf45e
dc-21 <cholahkuboko@gmail..com>
2024-04-29 10:14:43 +0200

    client stuff

--- client/src/components/Home/Bootcamps.tsx
+++ client/src/components/Home/Bootcamps.tsx
@@ -6,14 +6,21 @@ const Bootcamps = () => {
       <p className="md:text-[55px] text-[35px] font-bold text-[#06031d]">
         BootCamps For You
       </p>
-      <div className="p-6 grid grid-cols-2 justify-center items-center w-full gap-6">
+      <div className="md:p-6 p-2 grid md:grid-cols-2 grid-cols-1 justify-center items-center w-full gap-6">
         <img
           className="p-4 shadow shadow-black rounded-2xl"
           src={bootcamp}
           alt="bootcamp"
         />
-        <div>
-          <p>We bring you closer to world class bootcamps</p>
+        <div className="p-3 w-full">
+          <p className="text-3xl md:flex hidden text-slate-950 max-w-lg">
+            We bring you closer to world class bootcamps
+          </p>
+          <div className="flex w-full md:justify-start justify-center mt-4">
+            <button className="p-3 shadow shadow-gray-900 rounded-lg hover:rounded-xl bg-[#7d1af0] text-white text-lg hover:shadow-gray-700">
+              Enroll Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
